04f2f8395bfe8573141201a717866ad38418ec9c
owaisalam12 <owaisalamkhi@gmail.com>
2018-03-28 03:01:24 +0500

    Initial commit

---
+++ app/src/main/java/com/example/oways/seekbar/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.oways.seekbar;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer=MediaPlayer.create(this,R.raw.balin);
+
+
+        final SeekBar seekBar=(SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+            seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        },0,100);
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+    public void play(View view){
+        Button playBtn=(Button)findViewById(R.id.playButton);
+        mediaPlayer.start();
+
+    }
+}
